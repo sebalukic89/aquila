77f8918ca806af9c0cf3910d77851310b72fd0df
Sebastijan Lukic <lukicseba@gmail.com>
2021-11-08 20:03:17 -0500

    added the header.php and footer.php files

---
+++ footer.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Footer Template
+ *
+ * @package Aquila
+ */
+?>
+
+<footer>Footer</footer>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -5,21 +5,13 @@
  *
  * @package Aquila
  */
-?>
 
+get_header(); ?>
 
-<!DOCTYPE html>
-<html lang="en">
+<div class="content">
+    Some content
+</div>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Wordpress Theme</title>
-</head>
+<?php
 
-<body>
-
-</body>
-
-</html>
+get_footer();

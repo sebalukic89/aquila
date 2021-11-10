19baffd0ea7cedcf94de1a9da2a103b0f99cdb62
Sebastijan Lukic <lukicseba@gmail.com>
2021-11-09 22:06:27 -0500

    added the classes foler and other directories

--- functions.php
+++ functions.php
@@ -5,3 +5,39 @@
  * 
  * @package Aquila
  */
+
+use AQUILA_THEME\Inc\AQUILA_THEME;
+
+if (!defined('AQUILA_DIR_PATH')) {
+    define('AQUILA_DIR_PATH', untrailingslashit(get_template_directory()));
+}
+
+require_once AQUILA_DIR_PATH . '/inc/helpers/autoloader.php';
+
+function aquila_get_theme_instance()
+{
+    \AQUILA_THEME\Inc\AQUILA_THEME::get_instance();
+}
+
+aquila_get_theme_instance();
+
+function aquila_enqueue_scripts()
+{
+    // Resgister Styles
+    wp_register_style('aquila-css', get_stylesheet_uri(), [], filemtime(get_template_directory() . '/style.css'), 'all');
+    wp_register_style('bootstrap-css', get_template_directory_uri() . '/assets/src/library/css/bootstrap.min.css', [], false, 'all');
+
+    // Resgister Scripts
+    wp_register_script('aquila-js', get_template_directory_uri() . "/assets/main.js", [], filemtime(get_template_directory() . '/assets/main.js'), true);
+    wp_register_script('bootstrap-js', get_template_directory_uri() . "/assets/src/library/js/bootstrap.min.js", ['jquery'], false, true);
+
+    // Engueue Styles
+    wp_enqueue_style('aquila-stylesheet');
+    wp_enqueue_style('bootstrap-css');
+
+    // Engueue Scripts
+    wp_enqueue_script('aquila-js');
+    wp_enqueue_script('bootstrap-js');
+}
+
+add_action('wp_enqueue_scripts', 'aquila_enqueue_scripts');
--- header.php
+++ header.php
@@ -9,14 +9,27 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="<?php language_attributes(); ?>">
 
 <head>
-    <meta charset="UTF-8">
+    <meta charset="<?php bloginfo('charset'); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Wordpress Theme</title>
+    <?php wp_head(); ?>
 </head>
 
-<body>
-    <header>Header</header>
+<body <?php body_class(); ?>>
+
+    <?php
+    if (function_exists('wp_body_open')) {
+        wp_body_open();
+    }
+    ?>
+
+    <div class="site" id="page">
+        <header id="masthead" class="site-header" role="banner">
+            <?php get_template_part('template-parts/header/nav'); ?>
+        </header>
+
+        <div class="site-content" id="content">
